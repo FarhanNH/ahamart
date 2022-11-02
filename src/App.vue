fb5054a2300ed668a9f21c3ce2f878c090fea183
farhan.huda <farhan.huda@pactindo.com>
2022-11-02 07:18:58 +0700

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <v-app>
+    <div row id="navbar">
+      <Navbar />
+    </div>
     <sidebar />
     <v-main>
       <HelloWorld />
@@ -9,6 +12,7 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld";
+import Navbar from "./components/Navbar.vue";
 import Sidebar from "./components/Sidebar.vue";
 
 export default {
@@ -17,6 +21,7 @@ export default {
   components: {
     HelloWorld,
     Sidebar,
+    Navbar,
   },
 
   data: () => ({
@@ -24,3 +29,14 @@ export default {
   }),
 };
 </script>
+<style>
+#navbar {
+  position: absolute;
+  width: 100%;
+  top: 0;
+  left: 250px;
+}
+.v-main {
+  background-color: #f5f8fa;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-card>
+    <v-row>
+      <v-col md="4" class="pb-0">
+        <div class="d-flex ml-5 mt-5">
+          <v-text-field
+            label="Search"
+            prepend-inner-icon="mdi-magnify"
+            outlined
+            dense
+          ></v-text-field>
+          <v-btn height="40" class="ml-2" dark depressed color="#0095E8"
+            >Cari</v-btn
+          >
+        </div>
+      </v-col>
+      <v-col md="6" class="pb-0">
+        <div class="d-flex justify-end mt-1">
+          <v-list dense width="230" max-width="230">
+            <v-list-group
+              v-for="item in items"
+              :key="item.title"
+              v-model="item.active"
+              no-action
+            >
+              <template v-slot:activator>
+                <div slot="prependIcon" id="customIcon">DC</div>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title"> </v-list-item-title>
+                  <v-list-item-subtitle
+                    v-text="item.subtitle"
+                  ></v-list-item-subtitle>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item v-for="child in item.items" :key="child.title">
+                <v-list-item-content>
+                  <v-list-item-title v-text="child.title"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+          </v-list>
+        </div>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        items: [{ title: "Setttings" }, { title: "Logout" }],
+        title: "Dane Cooper",
+        subtitle: "Super Admin",
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+#customIcon {
+  background-color: #eaecf3;
+  border-radius: 50%;
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 5px 7px;
+  margin-right: 10px;
+}
+</style>
